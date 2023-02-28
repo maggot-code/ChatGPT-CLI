c564688b3a477cc6f43930b4556392a4f746cf5c
maggot_code <63178315+maggot-code@users.noreply.github.com>
2023-03-01 00:25:49 +0800

    Create gpt.go

---
+++ services/gpt.go
@@ -0,0 +1,27 @@
+/*
+ * @FilePath: /ChatGPT-CLI/services/gpt.go
+ * @Author: maggot-code
+ * @Date: 2023-03-01 00:03:06
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2023-03-01 00:25:36
+ * @Description:
+ */
+package services
+
+// c := gogpt.NewClient("your token")
+// ctx := context.Background()
+
+// req := gogpt.CompletionRequest{
+// 	Model:     gogpt.GPT3Ada,
+// 	MaxTokens: 5,
+// 	Prompt:    "Lorem ipsum",
+// }
+// resp, err := c.CreateCompletion(ctx, req)
+// if err != nil {
+// 	return
+// }
+// fmt.Println(resp.Choices[0].Text)
+
+func UseGptService() {}
+
+func DefineGptService(key string) {}
